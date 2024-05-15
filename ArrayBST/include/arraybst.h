7cc86d5d6320d79b4a33a810c7fdbbea6a4ece01
SauhardhaKafle <Sauhardhakafle0@gmail.com>
2024-05-15 06:23:15 +0100

    changes

---
+++ ArrayBST/include/arraybst.h
@@ -0,0 +1,26 @@
+#ifndef ArrayBST_h
+#define ArrayBST_h
+
+#include "BST.h"
+
+
+struct ArrayNode{
+
+    int key;
+    int value;
+
+    ArrayNode(int key, int value) : key(key), value(value){};
+};
+
+class ArrayBST : public BST{
+
+    public:
+
+
+
+    private:
+
+
+};
+
+#endif
