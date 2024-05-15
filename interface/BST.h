9aa1ec26a3d8d6493a6c314a3e55df221d585c06
SauhardhaKafle <Sauhardhakafle0@gmail.com>
2024-05-15 05:22:02 +0100

    Interface class implementation

---
+++ interface/BST.h
@@ -0,0 +1,21 @@
+
+#ifndef BST_H
+#define BST_H
+
+class BST{
+
+public:
+
+//Default constructor
+BST(){};
+
+virtual bool isEmpty()= 0;
+virtual void addBST(int data)=0;
+virtual bool remove(int key) = 0;
+virtual bool search(int key) = 0;
+
+~BST(){};
+};
+
+
+#endif
